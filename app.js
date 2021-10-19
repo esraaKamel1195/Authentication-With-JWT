d55a3e7f33465de7cf23e2e9e64f56f235cfa548
esraa kamel <esraa.kamel1811@gmail.com>
2021-10-19 23:10:28 +0200

    Create node server and connect to database

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+
+const app = express();
+
+const User = require("./model/user");
+
+app.use(express.json());
+
+// Register
+app.post("/register", (req, res) => {
+    // our register logic goes here...
+});
+
+// Login
+app.post("/login", (req, res) => {
+    // our login logic goes here
+});
+
+module.exports = app;
