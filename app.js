479054f698cafcc50870c5a4eaecbe526d2f04d8
esraa kamel <esraa.kamel1811@gmail.com>
2021-10-19 23:59:14 +0200

    Registeration api

--- app.js
+++ app.js
@@ -9,8 +9,45 @@ const User = require("./model/user");
 app.use(express.json());
 
 // Register
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
+    
     // our register logic goes here...
+    try {
+        const { first_name, last_name, email, password } = req.body;
+
+        if(!( email && first_name && last_name && password )) {
+            res.status(400).send("All inputs is required");
+        }
+
+        const oldUser = await User.findOne({ email });
+
+        if(oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
+        }
+
+        encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.Create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(),
+            password: encryptedPassword
+        });
+
+        const token = jWt.sign(
+            { user_id: user._id , email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h",
+            }
+        );
+
+        user.token = token;
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // Login
