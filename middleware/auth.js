0ccf50fe24e4a9cb57cc7a29273546af5a550b16
Esraa kamel <esraa.kamel1811@gmail.com>
2021-10-23 10:04:07 +0200

    Update middleware/auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,33 @@
 const jwt = require("jsonwebtoken");
 const config = process.env;
 
-const verfyToken = (req, res, next) => {
-    console.log(req.headers["x-access-token"]);
+const auth = (req, res, next) => {
     const token = req.body.token || req.query.token || req.headers["x-access-token"];
 
-    if(!token) {
-        return res.status(403).send("A token is required for authentication");
+    if(!token && !req.session.user) {
+        var err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
     }
-
-    try {
-        const decoded = jwt.verify( token, config.TOKEN_KEY );
-        req.user = decoded;
+    else {
+        if (req.session.user === 'authenticated') {
+            try {
+                const decoded = jwt.verify( token, config.TOKEN_KEY );
+                req.user = decoded;
+            }
+            catch(err) {
+               res.status(401).send("Invalid token");
+            }
+            next();
+        }
+        else {
+            var err = new Error('You are not authenticated!');
+            err.status = 403;
+            return next(err);
+        }
     }
-    catch(err) {
-       res.status(401).send("Invalid token");
-    }
+    
     return next();
 }
 
-module.exports = verfyToken;
+module.exports = auth;
