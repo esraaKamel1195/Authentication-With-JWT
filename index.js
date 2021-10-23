4af794f0fe618c86d34acd4ce8da1cb410716cc9
Esraa kamel <esraa.kamel1811@gmail.com>
2021-10-23 09:29:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,31 @@ const port = process.env.API_PORT || API_PORT;
 server.listen( port, ()=> {
     console.log(`Server running on port ${port}`);
 })
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
